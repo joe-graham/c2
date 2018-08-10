d218475b733271f4201a5771d2c7ebbe638dc34d
Joe Graham <jmgraham@sei.cmu.edu>
2018-08-10 14:37:13 -0400

    Finish implementing server

--- server/ssh-server.go
+++ server/ssh-server.go
@@ -1,34 +1,92 @@
 package main
 
 import (
-     "fmt"
-     "log"
+	"fmt"
+	"log"
+	"net"
 
-     "golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 func main() {
-     // Create very basic SSH server config
-     config := &ssh.ServerConfig{
-          PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
-               if c.User() == "c2user" && string(pass) == "c2password" {
-                    return nil, nil
-               }
-               return nil, fmt.Errorf("Password rejected, %q", c.User())
-          },
-     }
+	// Create very basic SSH server config
+	config := &ssh.ServerConfig{
+		PasswordCallback: func(c ssh.ConnMetadata, pass []byte) (*ssh.Permissions, error) {
+			if c.User() == "c2user" && string(pass) == "c2password" {
+				return nil, nil
+			}
+			return nil, fmt.Errorf("Password rejected, %q", c.User())
+		},
+	}
 
-     privateKeyString = "-----BEGIN EC PRIVATE KEY-----
+	privateKeyString := `-----BEGIN EC PRIVATE KEY-----
 MIHcAgEBBEIBgXU0TxInvi67PusMrMyWolN1z4BFx8AeN/zWXj2hXBbl42pBU/dM
 ZED/W9ZqjpsZUKF3vKNZ0hFROoztozx1+mKgBwYFK4EEACOhgYkDgYYABABdmQT3
 66DyKuNmRpTZC+R/dMq4e9+2Y1ytoe7ytrg4rL5TxZOcac1wieUIs0wQv6FHeajk
 ZTCyLqqsFJ5d8xXYagB5b2nlLJZIKf56TlAmgHgX2AelXkmCmWtnBMSWXTOKyF+1
 Uv/Vmfjc4SDQ6OPt0BNWTIP3t70Y64yK4ouUAigruA==
------END EC PRIVATE KEY-----"
-     privateKey, error = ssh.ParsePrivateKey([]byte(privateKeyString))
+-----END EC PRIVATE KEY-----`
+	privateKey, error := ssh.ParsePrivateKey([]byte(privateKeyString))
+	if error != nil {
+		log.Fatal("Failed to parse private key: ", error)
+	}
 
-     config.AddHostKey(privateKey)
+	config.AddHostKey(privateKey)
 
-     // Begin accepting connections
+	// Begin accepting connections
+	listener, error := net.Listen("TCP", "0.0.0.0:900")
+	if error != nil {
+		log.Fatal("Failed to listen for connection: ", error)
+	}
 
+	newConn, error := listener.Accept()
+	if error != nil {
+		log.Fatal("Failed to accept new connection: ", error)
+	}
+
+	// When new connection arrives, perform SSH handshake
+	_, channels, requests, error := ssh.NewServerConn(newConn, config)
+	if error != nil {
+		log.Fatal("Failed to handshake: ", error)
+	}
+
+	log.Printf("Handshake successful")
+
+	// Handle incoming connection concurrently
+	go ssh.DiscardRequests(requests)
+
+	for newChannel := range channels {
+		// There are different types of channels, we only want to deal with
+		// sessions.
+		if newChannel.ChannelType() != "session" {
+			newChannel.Reject(ssh.UnknownChannelType, "Unknown channel type")
+		}
+
+		channel, requests, error := newChannel.Accept()
+		if error != nil {
+			log.Fatal("Could not accept channel:", error)
+		}
+
+		// There are several types of out-of-band requests, we only want to
+		// deal with shells. Anonymous concurrent function.
+		go func(in <-chan *ssh.Request) {
+			for req := range in {
+				req.Reply(req.Type == "shell", nil)
+			}
+		}(requests)
+
+		term := terminal.NewTerminal(channel, ">")
+
+		go func() {
+			for {
+				line, error := term.ReadLine()
+				if error != nil {
+					break
+				}
+				fmt.Println(line)
+			}
+			channel.Close()
+		}()
+	}
 }
